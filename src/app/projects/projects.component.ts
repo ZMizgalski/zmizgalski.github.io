6842d1cda2e64b02f148520f900318bdac36436a
ZMizgalski <zbigniew.mizgalskifullstack@gmail.com>
2022-04-05 21:09:41 +0200

    update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,82 +9,82 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
   projects: Project[] = [
     {
-      name: 'Level-Crossing-Python-Server',
+      name: 'Level Crossing Python Server',
       link: 'https://github.com/ZMizgalski/Level-Crossing-Python-Server',
       description: '',
     },
     {
-      name: 'Level-Crossing-Python-Client',
+      name: 'Level Crossing Python Client',
       link: 'https://github.com/ZMizgalski/Level-Crossing-Python-Client',
       description: '',
     },
     {
-      name: 'Level-Crossing-Angular',
+      name: 'Level Crossing Angular',
       link: 'https://github.com/ZMizgalski/Level-Crossing-Angular',
       description: '',
     },
     {
-      name: 'Level-Crossing-Server-Side-Java',
+      name: 'Level Crossing Server Side Java',
       link: 'https://github.com/ZMizgalski/Level-Crossing-Server-Side-Java',
       description: '',
     },
     {
-      name: 'Level-Crossing-Client-Side-Java ',
+      name: 'Level Crossing Client Side Java ',
       link: 'https://github.com/ZMizgalski/Level-Crossing-Client-Side-Java',
       description: '',
     },
     {
-      name: 'Shop-Backend-Spring-Boot-Java',
+      name: 'Shop Backend Spring Boot Java',
       link: 'https://github.com/ZMizgalski/Shop-Backend-Spring-Boot-Java',
       description: '',
     },
     {
-      name: 'Simple-File-Upload-Angular',
+      name: 'Simple File Upload Angular',
       link: 'https://github.com/ZMizgalski/Simple-File-Upload-Angular',
       description: '',
     },
     {
-      name: 'Simple-File-Upload-Spring-Boot-Java',
+      name: 'Simple File Upload Spring Boot Java',
       link: 'https://github.com/ZMizgalski/Simple-File-Upload-Spring-Boot-Java',
       description: '',
     },
     {
-      name: 'Example-Data-Share-Angular',
+      name: 'Example Data Share Angular',
       link: 'https://github.com/ZMizgalski/Example-Data-Share-Angular',
       description: '',
     },
     {
-      name: 'Example-Data-Share-Spring-Boot-Java',
+      name: 'Example Data Share Spring Boot Java',
       link: 'https://github.com/ZMizgalski/Example-Data-Share-Spring-Boot-Java',
       description: '',
     },
     {
-      name: 'Example-Data-Share-Spring-Boot-Java',
+      name: 'Example Data Share Spring Boot Java',
       link: 'https://github.com/ZMizgalski/Example-Data-Share-Spring-Boot-Java',
       description: '',
     },
     {
-      name: 'Can-Sat-Website',
+      name: 'Can Sat Website',
       link: 'https://github.com/ZMizgalski/Can-Sat-Website',
       description: '',
     },
     {
-      name: 'Star-Wars-Example',
+      name: 'Star Wars Example',
       link: 'https://github.com/ZMizgalski/Star-Wars-Example',
       description: '',
     },
     {
-      name: 'Example-Portfolio-Website',
+      name: 'Example Portfolio Website',
       link: 'https://github.com/ZMizgalski/Example-Portfolio-Website',
       description: '',
     },
     {
-      name: 'Library-Spring-Boot-Java',
+      name: 'Library Spring Boot Java',
       link: 'https://github.com/ZMizgalski/Library-Spring-Boot-Java',
       description: '',
     },
     {
-      name: 'Library-Example-Angular',
+      name: 'Library Example Angular',
       link: 'https://github.com/ZMizgalski/Library-Example-Angular',
       description: '',
     },
